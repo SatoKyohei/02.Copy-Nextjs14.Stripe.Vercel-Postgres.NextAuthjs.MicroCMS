1535916f19ecb7541de090d015bc4c4bb91fba29
SatoKyohei <hkghsc3it@gmail.com>
2024-10-20 14:31:48 +0900

    更新

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,10 @@ import "./globals.css";
 import { Noto_Sans_JP } from "next/font/google";
 import Header from "@/app/components/Header";
 import { NextAuthProvider } from "@/app/lib/next-auth/provider";
-import { NextFontWithVariable } from "next/dist/compiled/@next/font";
 
 
 // 変数定義とエクスポート
-export const notoSansJP: NextFontWithVariable = Noto_Sans_JP({
+const notoSansJP = Noto_Sans_JP({
     subsets: ["latin"],
     variable: "--font-noto-sans-jp",
 });
