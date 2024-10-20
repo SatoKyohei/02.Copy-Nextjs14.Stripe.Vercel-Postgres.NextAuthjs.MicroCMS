e12f89f732019e2d99b3881063c37a2ad8e6154b
SatoKyohei <hkghsc3it@gmail.com>
2024-10-20 14:56:28 +0900

    更新

--- src/app/components/Book.tsx
+++ src/app/components/Book.tsx
@@ -1,13 +1,39 @@
 "use client";
 
 import { BookType } from "@/app/types/types";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 type BookProps = {
-    book: BookType
-}
+    book: BookType;
+};
 
 // eslint-disable-next-line react/display-name
 const Book = ({ book }: BookProps) => {
+    const [showModal, setShowModal] = useState(false);
+    const { data: session } = useSession();
+    const user = session?.user;
+    const router = useRouter()
+
+    const handlePurchaseClick = () => {
+        setShowModal(true);
+    };
+
+    const handleCancel = () => {
+        setShowModal(false);
+    };
+
+    const handlePurchaseConfirm = () => {
+        if(!user){
+            setShowModal(false)
+            // ログインページへリダイレクト
+            router.push("/login")
+        }else{
+            // stripeで決済する
+        }
+    };
+
     return (
         <>
             {/* アニメーションスタイル */}
@@ -28,7 +54,10 @@ const Book = ({ book }: BookProps) => {
             `}</style>
 
             <div className="flex flex-col items-center m-4">
-                <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+                <a
+                    onClick={handlePurchaseClick}
+                    className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none"
+                >
                     <Image
                         priority
                         src={book.thumbnail.url}
@@ -43,22 +72,30 @@ const Book = ({ book }: BookProps) => {
                             この本は○○...
                         </p>
                         <p className="mt-2 text-md text-slate-700">
-                            値段：{book.price }円
+                            値段：{book.price}円
                         </p>
                     </div>
                 </a>
 
-                {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
-          <div className="bg-white p-8 rounded-lg">
-            <h3 className="text-xl mb-4">本を購入しますか？</h3>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-              購入する
-            </button>
-            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              キャンセル
-            </button>
-          </div>
-        </div> */}
+                {showModal && (
+                    <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
+                        <div className="bg-white p-8 rounded-lg">
+                            <h3 className="text-xl mb-4">本を購入しますか？</h3>
+                            <button
+                                onClick={handlePurchaseConfirm}
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+                            >
+                                購入する
+                            </button>
+                            <button
+                                onClick={handleCancel}
+                                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                            >
+                                キャンセル
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
         </>
     );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { NextAuthProvider } from "@/app/lib/next-auth/provider";
 
 
 // 変数定義とエクスポート
-const notoSansJP = Noto_Sans_JP({
+export const notoSansJP = Noto_Sans_JP({
     subsets: ["latin"],
     variable: "--font-noto-sans-jp",
 });
