4e699f0acc12c39bf7a97f1aa42c3c0e872410af
SatoKyohei <hkghsc3it@gmail.com>
2024-10-20 14:25:58 +0900

    更新

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,10 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 
-// こうすることでホットリロードが走って何度もインスタンス化することを防げる
-// シングルトン
+// こうすることでホットリロードが走って何度もインスタンス化することを防げる。シングルトン？
 
-let prisma: PrismaClient;
+// 動画での定義。ここから
+// let prisma: PrismaClient;
 
+// const globalForPrisma = global as unknown as {
+//     prisma: PrismaClient | undefined;
+// };
+
+// if (!globalForPrisma.prisma) {
+//     globalForPrisma.prisma = new PrismaClient();
+// }
+
+// prisma = globalForPrisma.prisma;
+
+// export default prisma;
+
+// ここまで。letじゃなくてconstで定義しろというエラーが出るためコメントアウト
+
+// 修正した定義
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient | undefined;
 };
@@ -12,7 +27,6 @@ const globalForPrisma = global as unknown as {
 if (!globalForPrisma.prisma) {
     globalForPrisma.prisma = new PrismaClient();
 }
-
-prisma = globalForPrisma.prisma;
+const prisma: PrismaClient = globalForPrisma.prisma;
 
 export default prisma;
